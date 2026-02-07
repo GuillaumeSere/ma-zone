d44944911960880b350c3a0cbabc81d01153b715
Guillaume Sere <guillaumesere@hotmail.fr>
2026-02-07 09:53:10 +0100

    modif css

--- app/components/SiteFooter.tsx
+++ app/components/SiteFooter.tsx
@@ -2,7 +2,7 @@ export default function SiteFooter() {
   return (
     <footer className="border-t border-black/10 bg-linear-to-r from-rose-500 via-orange-500 to-amber-400 text-white">
       <div className="mx-auto max-w-6xl px-6 py-6 text-center text-xs font-semibold">
-        © {new Date().getFullYear()} Ma Zone. Tous droits réservés.
+        © {new Date().getFullYear()} Ma<span className="text-black">Zone</span> Tous droits réservés.
       </div>
     </footer>
   );
